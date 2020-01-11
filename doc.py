97dc63633799ba2dbd9ef5de74852a97d9ef8e67
damon_lin <825348451@qq.com>
2020-01-11 18:00:59 +0800

    doc.py

--- doc.py
+++ doc.py
@@ -1,24 +1,27 @@
 #conding=utf8  
 import os 
 
-#  - [backend](https://github.com/linsheng9731/notebook/tree/master/backend)
-
 ignoreList = ['out', 'bin', 'README.md', 'img', 'target']
+output = []
 def fileTree(dir, depth):
-    if depth > 2:
+    if depth > 1:
         return;
     entries = os.scandir(dir)
     for entry in entries:
         name = entry.name
-        if (name.endswith('.md', 3)) or ( (name not in ignoreList) and entry.is_dir() and not name.startswith('.')):
-            print(render(depth, entry.name))
+        if (name not in ignoreList) and (entry.is_dir() and not name.startswith('.')):
+            output.append(render(depth, entry.name))
         if entry.is_dir() and (not name.startswith('.')):
             fileTree(entry, depth+1)
 
 def tab(depth):
-    return ' ' * depth; 
+    return '  ' * depth; 
 
 def render(depth, name):
     return tab(depth) + "- [{}](https://github.com/linsheng9731/notebook/tree/master/{})".format(name, name)
-    
-fileTree('./', 1)
+
+fileTree('./', 0)
+fo = open("README.md", "w")
+fo.write("# Catalog\n")
+for e in output:
+    fo.write(e + "\n")
